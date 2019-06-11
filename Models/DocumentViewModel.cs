7ed5aaa90ecf8c36d5c31850bdb59a7da8b50e17
Ulises <upp_ulises@hotmail.com>
2019-06-11 12:19:38 -0500

    Add files via upload

---
+++ Models/DocumentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Question7.Models
+{
+    public class DocumentViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Title")]
+        [Required(ErrorMessage ="Title is required")]
+        public string DocumentName { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "Content")]
+        [Required(ErrorMessage ="Content is required")]
+        public string DocumentText { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
